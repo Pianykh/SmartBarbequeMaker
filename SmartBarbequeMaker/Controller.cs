109776fe5aa16b2d8df34ed2a42514a590f2fd74
Serhii Pianykh <pyanykhs@gmail.com>
2021-04-09 18:25:14 +0300

    added new function to get out the meat

--- SmartBarbequeMaker/Controller.cs
+++ SmartBarbequeMaker/Controller.cs
@@ -13,13 +13,14 @@ namespace SmartBarbequeMaker
 
             while (true)
             {
-                WriteLine("Choose action: 1. Load Meat in Grill 2. Press \"Pork\" 3. Press \"Chicken\" 4. Press\"Beef\"");
+                WriteLine("Choose action: 1. Load Meat in Grill 2. Press \"Pork\" " +
+                          $"3. Press \"Chicken\" 4. Press\"Beef\"  {(barbecue.LoadedMeat != null ? "5. Get the meat out" : "")}");
                 
                 switch (ReadLine())
                 {
                     case "1":
                         barbecue.LoadedMeat = new Beef();
-                        Console.WriteLine("Meat loaded");
+                        WriteLine("Meat loaded");
                         break;
                     case "2":
                         barbecue.CookPork();
@@ -29,6 +30,12 @@ namespace SmartBarbequeMaker
                         break;
                     case "4":
                         barbecue.CookBeef();
+                        break;
+                    case "5" when (barbecue.LoadedMeat != null) : barbecue.LoadedMeat = null;
+                        WriteLine("The meat is pulled out");
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect operation");
                         break;
                 }
             }
